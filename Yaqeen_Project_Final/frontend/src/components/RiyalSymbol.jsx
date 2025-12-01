eee9394bdb46846b8cc76c327232a4d157644f00
motairi2010 <motairi2010@gmail.com>
2025-12-02 01:30:36 +0300

    Updated package-lock.json

--- Yaqeen_Project_Final/frontend/src/components/RiyalSymbol.jsx
+++ Yaqeen_Project_Final/frontend/src/components/RiyalSymbol.jsx
@@ -1,23 +1,24 @@
-﻿import React from "react";
+import React from "react";
 import "../styles/riyal-system.css";
 
-const RiyalSymbol = ({ 
-  amount, 
-  showText = false, 
+const RiyalSymbol = ({
+  amount,
+  showText = false,
   size = "normal",
-  className = "" 
+  className = "",
+  style = {}
 }) => {
   const sizeClass = {
     small: "riyal-small",
-    normal: "riyal-normal", 
+    normal: "riyal-normal",
     large: "riyal-large"
   }[size];
 
   return (
-    <span className={`riyal-container ${sizeClass} ${className}`}>
-      <span className="riyal-symbol">ر</span>
+    <span className={`riyal-container ${sizeClass} ${className}`} style={style}>
+      <span className="riyal-symbol">﷼</span>
       {amount && <span className="riyal-amount">{amount}</span>}
-      {showText && <span className="riyal-text">ريال سعودي</span>}
+      {showText && <span className="riyal-text">﷼</span>}
     </span>
   );
 };
